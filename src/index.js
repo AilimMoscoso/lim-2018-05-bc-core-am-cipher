8f1acb20a00a4594db3456e8afe3337e475abc36
Ailim <ailim.moscoso@gmail.com>
2018-05-24 12:58:46 -0500

    24 de mayo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+function encriptar(elemento){
+    var mensaje = document.getElementsByTagName("mensaje")[0].value;
+    var mensaje_codificado = btoa(mensaje);
+    document.getElementById("resultado2").innerHTML = mensaje_codificado;
+}
+
 
