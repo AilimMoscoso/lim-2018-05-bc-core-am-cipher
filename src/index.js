45ed162c0f50a1468a754ad04ec2f32e4fb7c14e
AilimMoscoso <ailim.moscoso@gmail.com>
2018-05-24 22:56:43 -0500

    encriptacion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-function encriptar(elemento){
-    var mensaje = document.getElementsByTagName("mensaje")[0].value;
-    var mensaje_codificado = btoa(mensaje);
-    document.getElementById("resultado2").innerHTML = mensaje_codificado;
+function encriptar (elemento) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    //let message = document.getElementsByTagName("mensaje")[0].value;
+    //let offset = document.getElementsByÏd("offset").value;
+    let message = "hola mundo";
+    let offset = 10;
+    let messageCipher = "";
+    for (let i = 0; i < message.length; i++) {
+        let ind = alphabet.indexOf(message[i]);
+        if (ind == -1) messageCipher += message[i];
+        else messageCipher += alphabet[(ind + offset) % 26];
+    }
+    console.log(messageCipher);
+    //document.getElementById("resultado2").innerHTML = mensaje_codificado;
 }
-
-
