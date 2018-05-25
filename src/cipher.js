7cadc7b806a798a5bd2555fccab21c7d047fe212
Ailim <ailim.moscoso@gmail.com>
2018-05-25 11:03:10 -0500

    primera fase

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,12 @@
 window.cipher = {
-  // ... 
+  Encode: (string, offset)=>{ 
+    let messageCipher = " ";
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+  for (let i = 0; i < string.length; i++) {
+    let ind = alphabet.indexOf(string[i]);
+    if (ind == -1) messageCipher += string[i];
+    else messageCipher += alphabet[(ind + offset) % 26];
+}
+return messageCipher;
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-function encriptar (elemento) {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    //let message = document.getElementsByTagName("mensaje")[0].value;
-    //let offset = document.getElementsByÏd("offset").value;
-    let message = "hola mundo";
-    let offset = 10;
+function obtenerValor () {
+    //let alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let message = document.getElementById("message").value;
+    let offset=parseInt(document.getElementById("offset").value);
+    let salida1=window.cipher.Encode(message,offset);
+    document.getElementById("resultado2").value=salida1;
+    console.log(message);
+    console.log(offset);
+    console.log(salida1);
+}
+    
+    //.addEventListener("click", encriptar(){
+//document.getElementById("resultado2").innerHTML = messageCipher;
+    //});
+    /*
+    let offset = document.getElementsById("offset").value;
     let messageCipher = "";
+    
     for (let i = 0; i < message.length; i++) {
         let ind = alphabet.indexOf(message[i]);
         if (ind == -1) messageCipher += message[i];
@@ -12,4 +23,4 @@ function encriptar (elemento) {
     }
     console.log(messageCipher);
     //document.getElementById("resultado2").innerHTML = mensaje_codificado;
-}
+    */
