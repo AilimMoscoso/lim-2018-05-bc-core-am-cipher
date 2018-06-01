0a09f47e2dc9206744edf22d27201f4a61ef15d6
Ailim <ailim.moscoso@gmail.com>
2018-06-01 09:44:38 -0500

    readme

--- src/index.js
+++ src/index.js
@@ -9,30 +9,4 @@ document.getElementById('decodifica').addEventListener('click', () => {
   let mensajeDescifrado = window.cipher.decode(message.value, parseInt(offset.value));
   salida.value = mensajeDescifrado;
 });
-/*function obtenerValor (type) {
-    let message = document.getElementById("message").value;
-    let offset = parseInt(document.getElementById("offset").value);
-    let salida;
-    if (type === 'encode') salida = window.cipher.Encode(message,offset);
-    else salida = window.cipher.Decode(message,offset);
-    document.getElementById("resultado2").value=salida;
-    //console.log(message);
-    //console.log(offset);
-    //console.log(salida);
-}
- //let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    //.addEventListener("click", encriptar(){
-//document.getElementById("resultado2").innerHTML = messageCipher;
-    //});
-    /*
-    let offset = document.getElementsById("offset").value;
-    let messageCipher = "";
-    
-    for (let i = 0; i < message.length; i++) {
-        let ind = alphabet.indexOf(message[i]);
-        if (ind == -1) messageCipher += message[i];
-        else messageCipher += alphabet[(ind + offset) % 26];
-    }
-    console.log(messageCipher);
-    //document.getElementById("resultado2").innerHTML = mensaje_codificado;
-    */
+
